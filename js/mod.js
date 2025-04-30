77573fe21eea44d1b534379cafb8a607fabfc70b
Moonlight109e308 <3572586756@qq.com>
2025-04-30 23:56:33 +0800

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -12,14 +12,13 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+	num: "0.1",
+	name: "A New Beginning",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
 	<h3>v0.0</h3><br>
-		- Added things.<br>
-		- Added stuff.`
+		- The layer "magical energy" released.`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
